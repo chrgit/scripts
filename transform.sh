7968907c18ef038e4fee24b34adcf43a4280723e
Manolis Grampsas <43517497+manolisgrampsas01@users.noreply.github.com>
2024-04-12 01:10:36 +0300

    Experimental changes

--- transform.sh
+++ transform.sh
@@ -20,3 +20,23 @@ done
 chmod +x tax.sh
 ./tax.sh
 rm tax.sh
+echo -n
+echo "...and then!"
+echo
+echo "Taxonomy from 20 to 10"
+touch tax.sh
+for i in {20..10}; do
+    if (( i % 2 == 0 )); then
+        echo "$i is an even number"
+    else
+        echo "$i is an odd number"
+    fi
+done
+chmod +x tax.sh
+./tax.sh
+rm tax.sh
+echo -n
+echo "...and then!"
+echo
+echo "Taxonomy from 10 to 20"
+touch tax.sh
