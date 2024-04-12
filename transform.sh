6eb5fd44ea5e81e929bc9a556133c224a3a239ec
chrgr <gramchris@gmail.com>
2024-04-12 20:34:21 +0300

    transform.sh has been bigger

--- transform.sh
+++ transform.sh
@@ -6,17 +6,33 @@ chmod +x text.sh
 ./text.sh
 rm text.sh
 echo -n
+echo
 echo "...and then!"
 echo
-echo "Taxonomy from 25 to 30"
-touch tax.sh
-for i in {25..30}; do
-    if (( i % 2 == 0 )); then
-        echo "$i is an even number"
-    else
-        echo "$i is an odd number"
-    fi
+echo "Taxonomy from 1 to 10"
+echo
+touch tax1.sh
+for i in {1..10}; do
+  if (( i % 2 == 0 )); then
+    echo "$i is an even number"
+  else
+    echo "$i is an odd number"
+  fi
 done
-chmod +x tax.sh
-./tax.sh
-rm tax.sh 
+chmod +x tax1.sh
+./tax1.sh
+# rm tax.sh 
+echo
+echo "Taxonomy from 20 to 30"
+echo
+touch tax2.sh
+for i in {20..30}; do
+	if (( i % 2 == 0 )); then
+		echo "$i is an even number"
+	else
+		echo "$i is an odd number"
+	fi
+done
+chmod +x tax2.sh
+./tax2.sh
+# rm tax.sh
